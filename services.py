a69b71b25e85f3d09fd68924f261c7288f57861c
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-04-04 15:39:06 +0200

    a

--- services.py
+++ services.py
@@ -1,3 +1,16 @@
 
 def calculateSalary():
     pass
+
+def calculator(tal1, tal2, operator):
+    if operator == "+":
+        return tal1 + tal2
+    if operator == "-":
+        return tal1 - tal2        
+    if operator == "/":
+        return tal1 / tal2                
+    if operator == "*":
+        return tal1 * tal2                        
+    if operator == "%":
+        return tal1 % tal2                                
+    return None
